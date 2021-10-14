65af6922cfc5593becb7a8b3808717bd3ef59336
shinbaad <shinbaad@gmail.com>
2021-10-14 20:32:52 +0900

    add ISerial.cs

---
+++ kr.co.ztec.io/ISerial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kr.co.ztec.io
+{
+  public interface ISerial
+  {
+    Int32 Read(byte[] b, Int32 sz);
+  }
+}
--- kr.co.ztec.io/Serial.cs
+++ kr.co.ztec.io/Serial.cs
@@ -5,6 +5,8 @@ namespace kr.co.ztec.io
 {
   unsafe public partial class Serial
   {
+    private delegate Int32 OnCallbackRead(Int32 fd, byte[] b, Int32 sz);
+
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     private delegate Int32 DlgtOnRead(void* h, Int32 fd, byte* b, Int32 sz);
 
@@ -20,16 +22,20 @@ namespace kr.co.ztec.io
     [DllImport("libio.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
     private static extern Int32 serial_write(void* h, Int32 fd, byte* b, Int32 sz);
 
-
+    OnCallbackRead _callbackRead = null;
     DlgtOnRead _dlgtOnRead = null;
     UInt32 _dlgtPtr = 0;
     void* hdl = null;
     Int32 fd = 0;
-
-    public Serial()
+    ISerial _isr = null;
+    public Serial(ISerial _i)
     {
+      _isr = _i;
+
       _dlgtOnRead = new DlgtOnRead(OnRead);
       _dlgtPtr = (UInt32)Marshal.GetFunctionPointerForDelegate(_dlgtOnRead);
+
+      _callbackRead = new OnCallbackRead(CallbackRead);
     }
 
     public Int32 Open(string name, string baudrate, string databit, string stopbit, string parity)
@@ -53,7 +59,7 @@ namespace kr.co.ztec.io
       fixed (byte* __stopbit = _stopbit)
       fixed (byte* __parity = _parity)
       {
-        e = serial_open(out hdl, __name, __baudrate, __databit, __stopbit, __parity, _dlgtPtr, null);
+        this.fd = e = serial_open(out hdl, __name, __baudrate, __databit, __stopbit, __parity, _dlgtPtr, null);
       }
 
       return e;
@@ -62,24 +68,35 @@ namespace kr.co.ztec.io
     public Int32 Close()
     {
       Int32 e = 0;
-
+      serial_close(out hdl, this.fd);
       return e;
     }
 
     public Int32 Write(byte[] b, Int32 sz)
     {
       Int32 e = 0;
-
+      fixed (byte* _b = b)
+      {
+        e = serial_write(hdl, this.fd, _b, sz);
+      }
       return e;
     }
 
     private Int32 OnRead(void* h, Int32 fd, byte* b, Int32 sz)
     {
       Int32 e = 0;
-
+      byte[] _b = new byte[sz];
+      Marshal.Copy((IntPtr)b, _b, 0, sz);
+      _callbackRead.Invoke(fd, _b, sz);
       return e;
     }
 
+    private Int32 CallbackRead(Int32 fd, byte[] b, Int32 sz)
+    {
+      Int32 e = 0;
+      e = _isr.Read(b, sz);
+      return e;
+    }
 
   }
 
