72050095f248da4664b14abd8d591a137d80c84d
shinbaad <shinbaad@gmail.com>
2021-11-10 18:35:59 +0900

    add TextBox

--- ztecWApp/Form1.Designer.cs
+++ ztecWApp/Form1.Designer.cs
@@ -34,57 +34,98 @@ namespace ztecWApp
       this.hsbG = new System.Windows.Forms.HScrollBar();
       this.hsbB = new System.Windows.Forms.HScrollBar();
       this.hsbA = new System.Windows.Forms.HScrollBar();
+      this._txtA = new System.Windows.Forms.TextBox();
+      this._txtR = new System.Windows.Forms.TextBox();
+      this._txtG = new System.Windows.Forms.TextBox();
+      this._txtB = new System.Windows.Forms.TextBox();
       this.SuspendLayout();
       // 
       // _pnlSample
       // 
       this._pnlSample.BackColor = System.Drawing.Color.Black;
-      this._pnlSample.Location = new System.Drawing.Point(135, 12);
+      this._pnlSample.Location = new System.Drawing.Point(12, 12);
       this._pnlSample.Name = "_pnlSample";
-      this._pnlSample.Size = new System.Drawing.Size(240, 139);
+      this._pnlSample.Size = new System.Drawing.Size(431, 164);
       this._pnlSample.TabIndex = 1;
       // 
       // hsbR
       // 
-      this.hsbR.Location = new System.Drawing.Point(135, 214);
+      this.hsbR.Location = new System.Drawing.Point(12, 224);
       this.hsbR.Maximum = 255;
       this.hsbR.Name = "hsbR";
-      this.hsbR.Size = new System.Drawing.Size(240, 21);
+      this.hsbR.Size = new System.Drawing.Size(328, 21);
       this.hsbR.TabIndex = 2;
       this.hsbR.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hsbR_Scroll);
       // 
       // hsbG
       // 
-      this.hsbG.Location = new System.Drawing.Point(135, 250);
+      this.hsbG.Location = new System.Drawing.Point(12, 260);
       this.hsbG.Maximum = 255;
       this.hsbG.Name = "hsbG";
-      this.hsbG.Size = new System.Drawing.Size(240, 21);
+      this.hsbG.Size = new System.Drawing.Size(328, 21);
       this.hsbG.TabIndex = 2;
       this.hsbG.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hsbG_Scroll);
       // 
       // hsbB
       // 
-      this.hsbB.Location = new System.Drawing.Point(135, 283);
+      this.hsbB.Location = new System.Drawing.Point(12, 293);
       this.hsbB.Maximum = 255;
       this.hsbB.Name = "hsbB";
-      this.hsbB.Size = new System.Drawing.Size(240, 21);
+      this.hsbB.Size = new System.Drawing.Size(328, 21);
       this.hsbB.TabIndex = 2;
       this.hsbB.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hsbB_Scroll);
       // 
       // hsbA
       // 
-      this.hsbA.Location = new System.Drawing.Point(135, 179);
+      this.hsbA.Location = new System.Drawing.Point(12, 191);
       this.hsbA.Maximum = 255;
       this.hsbA.Name = "hsbA";
-      this.hsbA.Size = new System.Drawing.Size(240, 21);
+      this.hsbA.Size = new System.Drawing.Size(328, 21);
       this.hsbA.TabIndex = 2;
       this.hsbA.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hsbA_Scroll);
+      // 
+      // _txtA
+      // 
+      this._txtA.Location = new System.Drawing.Point(343, 192);
+      this._txtA.Name = "_txtA";
+      this._txtA.Size = new System.Drawing.Size(100, 20);
+      this._txtA.TabIndex = 3;
+      this._txtA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      this._txtA.TextChanged += new System.EventHandler(this._txtA_TextChanged);
+      // 
+      // _txtR
+      // 
+      this._txtR.Location = new System.Drawing.Point(343, 224);
+      this._txtR.Name = "_txtR";
+      this._txtR.Size = new System.Drawing.Size(100, 20);
+      this._txtR.TabIndex = 3;
+      this._txtR.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      // 
+      // _txtG
+      // 
+      this._txtG.Location = new System.Drawing.Point(343, 260);
+      this._txtG.Name = "_txtG";
+      this._txtG.Size = new System.Drawing.Size(100, 20);
+      this._txtG.TabIndex = 3;
+      this._txtG.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      // 
+      // _txtB
+      // 
+      this._txtB.Location = new System.Drawing.Point(343, 293);
+      this._txtB.Name = "_txtB";
+      this._txtB.Size = new System.Drawing.Size(100, 20);
+      this._txtB.TabIndex = 3;
+      this._txtB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(455, 329);
+      this.Controls.Add(this._txtB);
+      this.Controls.Add(this._txtG);
+      this.Controls.Add(this._txtR);
+      this.Controls.Add(this._txtA);
       this.Controls.Add(this.hsbB);
       this.Controls.Add(this.hsbG);
       this.Controls.Add(this.hsbA);
@@ -93,6 +134,7 @@ namespace ztecWApp
       this.Name = "Form1";
       this.Text = "Form1";
       this.ResumeLayout(false);
+      this.PerformLayout();
 
     }
 
@@ -102,6 +144,10 @@ namespace ztecWApp
     private System.Windows.Forms.HScrollBar hsbG;
     private System.Windows.Forms.HScrollBar hsbB;
     private System.Windows.Forms.HScrollBar hsbA;
+    private System.Windows.Forms.TextBox _txtA;
+    private System.Windows.Forms.TextBox _txtR;
+    private System.Windows.Forms.TextBox _txtG;
+    private System.Windows.Forms.TextBox _txtB;
   }
 }
 
--- ztecWApp/Form1.cs
+++ ztecWApp/Form1.cs
@@ -16,6 +16,8 @@ namespace ztecWApp
     public Form1()
     {
       InitializeComponent();
+      this.SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
+      this.UpdateStyles();
 
       ARGB[0] = 0;
       ARGB[1] = 0;
@@ -33,25 +35,34 @@ namespace ztecWApp
     private void hsbR_Scroll(object sender, ScrollEventArgs e)
     {
       ARGB[1] = (byte)hsbR.Value;
+      _txtR.Text = String.Format("{0:X02}", ARGB[1]);
       onChangeColor();
     }
 
     private void hsbG_Scroll(object sender, ScrollEventArgs e)
     {
       ARGB[2] = (byte)hsbG.Value;
+      _txtG.Text = String.Format("{0:X02}", ARGB[2]);
       onChangeColor();
     }
 
     private void hsbB_Scroll(object sender, ScrollEventArgs e)
     {
       ARGB[3] = (byte)hsbB.Value;
+      _txtB.Text = String.Format("{0:X02}", ARGB[3]);
       onChangeColor();
     }
 
     private void hsbA_Scroll(object sender, ScrollEventArgs e)
     {
       ARGB[0] = (byte)hsbA.Value;
+      _txtA.Text = String.Format("{0:X02}", ARGB[0]);
       onChangeColor();
+    }
+
+    private void _txtA_TextChanged(object sender, EventArgs e)
+    {
+
     }
   }
 }
